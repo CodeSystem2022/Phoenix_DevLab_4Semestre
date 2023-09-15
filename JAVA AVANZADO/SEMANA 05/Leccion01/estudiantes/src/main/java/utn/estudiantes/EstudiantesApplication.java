fb0e9984a4ac3a64eaaea3c3b318b2806b5fda2c
juani sorato <juanisorato21@gmail.com>
2023-09-15 19:18:31 -0300

    Actualizo Java Avanzado S5

---
+++ JAVA AVANZADO/SEMANA 05/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -0,0 +1,50 @@
+package utn.estudiantes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class EstudiantesApplication implements CommandLineRunner {
+
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
+
+
+	public static void main(String[] args) {
+
+		logger.info("Iniciando la aplicación...");
+		//Levantar la fabrica de Spring
+		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info("Aplicación finalizada!");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(nl+"Ejecutando el método run de Spring..."+nl);
+	}
+
+	@Override
+	public static void main(String[] args) {
+		logger.info(nl+"Ejecutando el método run de Spring..."+nl);
+		var salir = false;
+		var consola = new Scanner((System.in));
+		while(!salir){
+			mostrarMenu();
+			salir = ejecutarOpciones(consola);
+			logger.info(nl);
+		}//fin ciclo while
+
+	}
+}
+
+
