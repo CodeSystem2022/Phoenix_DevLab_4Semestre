0ace86493ef9b897ed56425b38e7a7c616a2ea54
Guzce <103860258+Guzce@users.noreply.github.com>
2023-09-20 21:31:33 -0300

    5.2 Dunder doc (__doc__) -> parte 1 y 2
    #  Correción en la extensión de archivo.

--- PYTHON/SEMANA 4/doc_str.py
+++ PYTHON/SEMANA 4/doc_str.py
@@ -1,6 +1,8 @@
-"""
-Podemos crear documentación
-"""
-"""
-estas son las dos maneras de documentar
-"""
+from mi_clase import MiClase
+
+# help(MiClase)
+# print(MiClase.__doc__)  # Otra forma de acceder/ llamado único a la documentación
+# print(MiClase.__init__.__doc__)
+# print(MiClase.mi_metodo.__doc__)   #Cada metodo en si mismo es un objeto
+print(MiClase.mi_metodo)  # Dirección del método
+print(type(MiClase.mi_metodo))
