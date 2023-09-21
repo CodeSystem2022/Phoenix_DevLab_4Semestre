e2bdf1197b963e9df7e0a78e6e5b5ffe5898316d
juani sorato <juanisorato21@gmail.com>
2023-09-20 23:10:27 -0300

    Node.js Semana 6

---
+++ Node.js/SEMANA 06/01-hola-mundo/app.js
@@ -0,0 +1,16 @@
+/////////////////////////////////
+//  let nombre = 'Juan';
+//  console.log(nombre);
+
+
+//  nombre = 'Martin';
+//  console.log(nombre);
+//////////////////////////////////
+
+const nombre = 'Juan Ignacio';
+console.log(nombre);
+
+const nombre2 = "Monica";
+console.log(nombre2);
+
+console.log('Hola Mundo desde Node');
