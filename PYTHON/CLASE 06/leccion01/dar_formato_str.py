c6f24658a6afbf9f6d13d652a08b3dbb155191ec
juani sorato <juanisorato21@gmail.com>
2023-09-25 18:29:11 -0300

    6.2 Avanzamos desde una tupla

--- PYTHON/CLASE 06/leccion01/dar_formato_str.py
+++ PYTHON/CLASE 06/leccion01/dar_formato_str.py
@@ -3,4 +3,11 @@
 nombre = 'Juan'
 edad = 20
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años'%(nombre, edad)
-print(mensaje_con_formato)
+
+# Creamos una tupla
+persona = ('Mariela', 'Guerrero', 5000.00)
+mensaje_con_formato = 'Hola %s %s .Tu sueldo es %.2f' # % persona --> Aquí le pasamos el objeto que es tupla
+print(mensaje_con_formato % persona)
+
+
+
