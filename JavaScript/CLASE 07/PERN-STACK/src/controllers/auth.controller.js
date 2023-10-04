36fa60759afdfb5b2aa00d0390ec05bc29c65bde
Guzce <103860258+Guzce@users.noreply.github.com>
2023-10-04 00:58:53 -0300

    7.1 Registro 7.2 Utilización de bcrypt

---
+++ JavaScript/CLASE 07/PERN-STACK/src/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import {pool} from "../db.js"
+import bcrypt from "bcrypt";
+
+export const signin = (req, res)=> res.send('ingresando');
+
+export const signup = async(req, res)=> {
+    const {name, email, password} = req.body;
+
+    console.log(result);
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+        const result = await pool.query("INSERT INTO usuarios {name, email, password} VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword])
+        console.log(result);
+        return res.json(result.rows[0]);
+
+    } catch (error) {
+        if(error.code === "23505"){
+            return res.status(400).json({message: "El correo ya esta registrado"});
+        }
+        
+    }
+};
+
+export const signout = (req, res) => res.send('Cerrando sesion');
+
+export const profile = (req, res) => res.send("Perfil de ususario");
