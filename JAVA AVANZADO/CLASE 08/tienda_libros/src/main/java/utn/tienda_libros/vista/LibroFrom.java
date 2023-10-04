f1e99565c8c19172cbeff37a16af1d64d1353cc1
juani sorato <juanisorato21@gmail.com>
2023-10-04 18:44:57 -0300

    8.1 Continuamos con el constructor y el método iniciarForma()

---
+++ JAVA AVANZADO/CLASE 08/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -0,0 +1,34 @@
+package utn.tienda_libros.vista;
+
+import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import utn.tienda_libros.servicio.LibroServicio;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Component
+public class LibroFrom extends JFrame {
+    LibroServicio libroServicio;
+    private JPanel panel;
+
+    @Autowired
+    public LibroFrom(LibroServicio libroServicio){
+        this.libroServicio = libroServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900, 700);
+        //Para obtener las dimenciones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);
+    }
+}
--- JavaScript/CLASE 06/PERN-stack/src/controllers/auth.controller.js
+++ JavaScript/CLASE 06/PERN-stack/src/controllers/auth.controller.js
@@ -1,6 +1,14 @@
+import { Pool } from "../db.js";
+
+
 export const signin = (req, res)=> res.send('ingresando');
 
-export const signup = (req, res)=> res.send('registrando');
+export const signup = (req, res)=> {
+    const{username, email, password}= req.body;
+    res.send("Registrando")
+
+    pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3)", [name, email, password])
+};
 
 export const signout = (req, res)=> res.send('Cerrando sesion');
 
