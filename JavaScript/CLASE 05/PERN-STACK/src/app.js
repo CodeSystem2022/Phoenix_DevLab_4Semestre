80b805966a26893f1f469566c7ff8b0b7a11c785
juan ignacio sorato <101840362+Juanisorato21@users.noreply.github.com>
2023-10-03 22:58:17 -0300

    Update app.js

--- JavaScript/CLASE 05/PERN-STACK/src/app.js
+++ JavaScript/CLASE 05/PERN-STACK/src/app.js
@@ -7,6 +7,7 @@ const app = express();
 //Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+
 app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res)=> res.json({message: "Bienvenido a mi proyecto"}));
