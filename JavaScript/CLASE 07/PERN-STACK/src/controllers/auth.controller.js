7c81d2b91ac4256da0b415f4072c8c0060e65ab9
Guzce <103860258+Guzce@users.noreply.github.com>
2023-10-04 01:41:12 -0300

    Cookies -> Guardar el token en una cookie

--- JavaScript/CLASE 07/PERN-STACK/src/controllers/auth.controller.js
+++ JavaScript/CLASE 07/PERN-STACK/src/controllers/auth.controller.js
@@ -16,13 +16,16 @@ export const signup = async(req, res)=> {
         const result = await pool.query("INSERT INTO usuarios {name, email, password} VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword])
         
         const token = await createAccessToken({id: result.rows[0].id});
-
         console.log(result);
-        // return res.json(result.rows[0]);
-        return res.json({
-            token: token,
+        res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "none",
+        maxAge: 60 * 60 * 24 * 1000 // 1 day
         });
 
+        console.log(result);
+        return res.json(result.rows[0]);
+        
     } catch (error) {
         if(error.code === "23505"){
             return res.status(400).json({message: "El correo ya esta registrado"});
