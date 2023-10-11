e68d230995fe4049039846df3e5a48623594b91d
juani sorato <juanisorato21@gmail.com>
2023-10-11 20:33:11 -0300

    JS video 8.1 y 8.2

--- Proyectos Tutores/Daniel Guerrero/PERN-stack/src/controllers/auth.controller.js
+++ Proyectos Tutores/Daniel Guerrero/PERN-stack/src/controllers/auth.controller.js
@@ -1,10 +1,37 @@
 import { pool } from "../db.js"
 import bcrypt from 'bcrypt'; //importamos bcrypt
 import { createAccessToken } from "../libs/jwt.js";
+import { json } from "express";
 
-export const signin = (req, res) => res.send('ingresando');
+export const signin = async (req, res) => { 
+    const {email, password} = req.body;
 
-export const signup = async (req, res) => {
+    const result = await pool.query("SELECT * FROM usuarios WHERE email = $1", [email]);
+
+    if (result.rowCount === 0) {
+        return res.status(400).json({message: "El correo no esta registrado"});
+    }
+
+    const validPassword = await bcrypt.compare(password, result.rows[0].password);
+
+    if (!validPassword) {
+        return res.status(400).json({message: "La contraseña es incorrecta"});
+    }
+
+    const token = await createAccessToken({id: result.rows[0].id });
+    console.log(result);
+    res.cookie("token", token,{
+        httpOnly: true,
+        secure: false,
+        samSite: "none",
+        maxAge: 60 *60 *24 * 1000,}) // 1day})
+    return res.json(result.rows[0]);
+
+
+
+};
+
+export const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     try {
@@ -26,10 +53,17 @@ export const signup = async (req, res) => {
         if(error.code === "23505"){
             return res.status(400).json({message: "El correo ya esta registrado"})
         }
+        next (error);
 
     }
 };
-export const signout = (req, res) => res.send('Cerrando sesion');
+export const signout = (req, res) => {
+    res.clearCookie("token");
+    return res.json({message: "Sesion cerrada"});
+};
+    
 
-export const profile = (req, res) => res.send('Perfil del usuario');
-
+export const profile = async (req, res) => {
+    const result = await pool.query('SELECT * FROM usuarios WHERE id = $1', [req.usuarioId]);
+    return res.json(result.rows[0]);
+}
