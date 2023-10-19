7df3b21fdd2b7861ac69026186a39c69a50a4a11
juani sorato <juanisorato21@gmail.com>
2023-10-19 20:48:37 -0300

    10.2 Aplicando Promise en la función hola -> debajo pongo el enlace a ES6

---
+++ Node.js/SEMANA 10/02-Fundamentos_node/async/promises.js
@@ -0,0 +1,28 @@
+function hola(nombre) { 
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () { 
+            console.log("Hola" + nombre); 
+            resolve(nombre); 
+        }, 1000); 
+    });
+  } 
+   
+  function hablar(miCallbackHablar) { 
+    setTimeout(function () { 
+      console.log("bla bla bla bla"); 
+      miCallbackHablar(); 
+    }, 1000); 
+  } 
+   
+  function adios(nombre, otroCallback) { 
+    setTimeout(function () { 
+      console.log("Adios " + nombre); 
+      otroCallback(); 
+    }, 1500); 
+  } 
+
+  console.log('Iniciando el proceso');
+  hola('Ariel')
+    .then((nombre)=> {
+        console.log('Terminando el proceso');
+    })
