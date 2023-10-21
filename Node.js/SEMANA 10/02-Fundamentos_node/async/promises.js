4e4a992066c26817799bc54340984885dffff35d
Guzce <103860258+Guzce@users.noreply.github.com>
2023-10-20 21:15:22 -0300

    10.3 Aplicando Promise en la función adios

--- Node.js/SEMANA 10/02-Fundamentos_node/async/promises.js
+++ Node.js/SEMANA 10/02-Fundamentos_node/async/promises.js
@@ -1,28 +1,40 @@
 function hola(nombre) { 
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () { 
-            console.log("Hola" + nombre); 
-            resolve(nombre); 
-        }, 1000); 
-    });
-  } 
-   
-  function hablar(miCallbackHablar) { 
-    setTimeout(function () { 
-      console.log("bla bla bla bla"); 
-      miCallbackHablar(); 
-    }, 1000); 
-  } 
-   
-  function adios(nombre, otroCallback) { 
-    setTimeout(function () { 
-      console.log("Adios " + nombre); 
-      otroCallback(); 
-    }, 1500); 
-  } 
+  return new Promise(function (resolve, reject) {
+      setTimeout(function () { 
+          console.log("Hola" + nombre); 
+          resolve(nombre); 
+      }, 1000); 
+  });
+} 
+ 
+function hablar(miCallbackHablar) { 
+  setTimeout(function () { 
+    console.log("bla bla bla bla"); 
+    miCallbackHablar(); 
+  }, 1000); 
+} 
+ 
+// function adios(nombre, otroCallback) { 
+//   setTimeout(function () { 
+//     console.log("Adios " + nombre); 
+//     otroCallback(); 
+//   }, 1500); 
+// } 
 
-  console.log('Iniciando el proceso');
-  hola('Ariel')
-    .then((nombre)=> {
-        console.log('Terminando el proceso');
-    })
+function adios(nombre) { 
+  return new Promise((resolve, reject) =>{
+    setTimeout(function () { 
+      console.log("Adios " + nombre); //con coma nodemon tmb interpreta pero es recomendado concatenar con signo +
+      resolve(); 
+    // Sintaxis Es6
+    }, 1000); 
+  })
+}
+
+//Llamamos a la función
+console.log('Iniciando el proceso');
+hola('Ariel')
+  .then(adios)
+  .then((nombre)=> {
+      console.log('Terminando el proceso');
+  })
